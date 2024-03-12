635c69a28d2adfce7db4b8923516f0ed6d9a89dc
waringhu <waringhu@163.com>
2024-03-12 14:15:15 +0800

    Initial Commit

---
+++ Reeder/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Reeder
+//
+//  Created by niein on 2024/3/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Reeder/ReederApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReederApp.swift
+//  Reeder
+//
+//  Created by niein on 2024/3/12.
+//
+
+import SwiftUI
+
+@main
+struct ReederApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
